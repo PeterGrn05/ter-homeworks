53573e77b7eac7abe5ef160fcdd44bb7ef62b2a6
Горин Пётр <peter14102005@gmail.com>
2026-09-07 15:35:39 +0300

    Задание 2 файлы

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -8,21 +8,32 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
+resource "yandex_vpc_subnet" "develop_db" {
+  name           = "develop-db"
+  zone           = var.vm_db_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = ["10.0.2.0/24"]
+}
+
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_image_family
 }
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standart-v4"
+  name                      = var.vm_web_name
+  platform_id               = var.vm_web_platform_id
+  zone                      = var.default_zone
+  allow_stopping_for_update = true
   resources {
-    cores         = 1
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vms_resources["web"].cores
+    memory        = var.vms_resources["web"].memory
+    core_fraction = var.vms_resources["web"].core_fraction
   }
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
+      size     = var.vms_resources["web"].hdd_size
+      type     = var.vms_resources["web"].hdd_type
     }
   }
   scheduling_policy {
@@ -33,9 +44,43 @@ resource "yandex_compute_instance" "platform" {
     nat       = true
   }
 
-  metadata = {
-    serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  metadata = merge(
+    var.metadata,
+    {
+      ssh-keys = "ubuntu:${var.vms_ssh_root_key}"
+    }
+  )
+}
+
+resource "yandex_compute_instance" "platform_db" {
+  name                      = var.vm_db_name
+  platform_id               = var.vm_db_platform_id
+  zone                      = var.vm_db_zone
+  allow_stopping_for_update = true
+  resources {
+    cores         = var.vms_resources["db"].cores
+    memory        = var.vms_resources["db"].memory
+    core_fraction = var.vms_resources["db"].core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+      size     = var.vms_resources["db"].hdd_size
+      type     = var.vms_resources["db"].hdd_type
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop_db.id
+    nat       = true
   }
 
+  metadata = merge(
+    var.metadata,
+    {
+      ssh-keys = "ubuntu:${var.vms_ssh_root_key}"
+    }
+  )
 }
--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -33,6 +33,77 @@ variable "vpc_name" {
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN3Hy4IhTpdLMXsoPbCM+roARN0ydkQBG8P7zwquYD5Q peter14102005@gmail.com"
   description = "ssh-keygen -t ed25519"
+}
+
+variable "vm_db_zone" {
+  type    = string
+  default = "ru-central1-b"
+}
+
+variable "vm_web_image_family" {
+  type    = string
+  default = "ubuntu-2004-lts"
+}
+
+variable "vm_db_image_family" {
+  type    = string
+  default = "ubuntu-2004-lts"
+}
+
+variable "vm_web_name" {
+  type    = string
+  default = "netology-develop-platform-web"
+}
+
+variable "vm_db_name" {
+  type    = string
+  default = "netology-develop-platform-db"
+}
+
+variable "vm_web_platform_id" {
+  type    = string
+  default = "standard-v3"
+}
+
+variable "vm_db_platform_id" {
+  type    = string
+  default = "standard-v3"
+}
+
+variable "vms_resources" {
+  type = map(object({
+    cores         = number
+    memory        = number
+    core_fraction = number
+    hdd_size      = number
+    hdd_type      = string
+  }))
+
+  default = {
+    web = {
+      cores         = 2
+      memory        = 2
+      core_fraction = 20
+      hdd_size      = 10
+      hdd_type      = "network-hdd"
+    }
+
+    db = {
+      cores         = 2
+      memory        = 4
+      core_fraction = 20
+      hdd_size      = 10
+      hdd_type      = "network-hdd"
+    }
+  }
+}
+
+variable "metadata" {
+  type = map(string)
+
+  default = {
+    serial-port-enable = "1"
+  }
 }
